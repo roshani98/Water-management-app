97c77266cc4a990bdafb6181de3b8763a7a9ac1d
nishi1612 <doshinishi1612@gmail.com>
2018-06-08 17:41:28 +0530

    Backend Update
    Added the feature of changing area , society , password , pincode for admin in  admin_change_details. Update the UI for the same activity. On backend it is changing the deatils properly.

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -54,21 +54,21 @@ public final class R {
     public static final class id {
         public static final int adjust_height = 0x7f090021;
         public static final int adjust_width = 0x7f090022;
-        public static final int auto = 0x7f090030;
-        public static final int button = 0x7f090036;
-        public static final int center = 0x7f09003b;
-        public static final int dark = 0x7f09004c;
-        public static final int date = 0x7f09004d;
-        public static final int icon_only = 0x7f09006c;
-        public static final int light = 0x7f090074;
-        public static final int none = 0x7f09008a;
-        public static final int normal = 0x7f09008b;
-        public static final int radio = 0x7f090098;
-        public static final int standard = 0x7f0900c0;
-        public static final int text = 0x7f0900c6;
-        public static final int text2 = 0x7f0900c7;
-        public static final int wide = 0x7f0900eb;
-        public static final int wrap_content = 0x7f0900ee;
+        public static final int auto = 0x7f090034;
+        public static final int button = 0x7f09003a;
+        public static final int center = 0x7f09003f;
+        public static final int dark = 0x7f090050;
+        public static final int date = 0x7f090051;
+        public static final int icon_only = 0x7f090070;
+        public static final int light = 0x7f090078;
+        public static final int none = 0x7f09008e;
+        public static final int normal = 0x7f09008f;
+        public static final int radio = 0x7f09009c;
+        public static final int standard = 0x7f0900c4;
+        public static final int text = 0x7f0900cb;
+        public static final int text2 = 0x7f0900cc;
+        public static final int wide = 0x7f0900f0;
+        public static final int wrap_content = 0x7f0900f3;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f0a0007;
