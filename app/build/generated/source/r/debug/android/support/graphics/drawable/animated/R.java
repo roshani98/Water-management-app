ea4f5ad0757b776addac9311a6a2469b4f4ecbf4
nishi1612 <doshinishi1612@gmail.com>
2018-06-20 15:10:00 +0530

    Revert "Revert "Revert "Revert "Revert "Merge remote-tracking branch 'upstream/master'"""""
    This reverts commit 5f66cf52b5b64c4ae5baae15d2e28d3e277d49b8.

--- app/build/generated/source/r/debug/android/support/graphics/drawable/animated/R.java
+++ app/build/generated/source/r/debug/android/support/graphics/drawable/animated/R.java
@@ -85,10 +85,10 @@ public final class R {
         public static final int notification_main_column_container = 0x7f090090;
         public static final int right_icon = 0x7f09009c;
         public static final int right_side = 0x7f09009d;
-        public static final int text = 0x7f0900d7;
-        public static final int text2 = 0x7f0900d8;
-        public static final int time = 0x7f090116;
-        public static final int title = 0x7f090117;
+        public static final int text = 0x7f0900d1;
+        public static final int text2 = 0x7f0900d2;
+        public static final int time = 0x7f0900f8;
+        public static final int title = 0x7f0900f9;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f0a000a;
