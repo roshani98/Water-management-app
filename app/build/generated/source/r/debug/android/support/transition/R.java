3ae1aa90ec5771f5377eb486d70ed50fe36a98fc
nishi1612 <doshinishi1612@gmail.com>
2018-06-20 15:10:31 +0530

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Merge remote-tracking branch 'upstream/master'""""""""""
    This reverts commit 73ddbe20380dcd6ceaa95105099f5863538b20df.

--- app/build/generated/source/r/debug/android/support/transition/R.java
+++ app/build/generated/source/r/debug/android/support/transition/R.java
@@ -98,15 +98,15 @@ public final class R {
         public static final int save_non_transition_alpha = 0x7f09009f;
         public static final int save_scale_type = 0x7f0900a0;
         public static final int status_bar_latest_event_content = 0x7f0900c4;
-        public static final int text = 0x7f0900d1;
-        public static final int text2 = 0x7f0900d2;
-        public static final int time = 0x7f0900f8;
-        public static final int title = 0x7f0900f9;
-        public static final int transition_current_scene = 0x7f0900ff;
-        public static final int transition_layout_save = 0x7f090100;
-        public static final int transition_position = 0x7f090101;
-        public static final int transition_scene_layoutid_cache = 0x7f090102;
-        public static final int transition_transform = 0x7f090103;
+        public static final int text = 0x7f0900d7;
+        public static final int text2 = 0x7f0900d8;
+        public static final int time = 0x7f090116;
+        public static final int title = 0x7f090117;
+        public static final int transition_current_scene = 0x7f09011d;
+        public static final int transition_layout_save = 0x7f09011e;
+        public static final int transition_position = 0x7f09011f;
+        public static final int transition_scene_layoutid_cache = 0x7f090120;
+        public static final int transition_transform = 0x7f090121;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f0a0004;
